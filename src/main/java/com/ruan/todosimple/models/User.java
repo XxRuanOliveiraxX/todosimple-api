c3fbd7e8248d0f1d6dbdbacdeda856ee266434ae
XxRuanOliveiraxX <162846693+XxRuanOliveiraxX@users.noreply.github.com>
2025-05-12 22:35:14 -0300

    UserModel

---
+++ src/main/java/com/ruan/todosimple/models/User.java
@@ -0,0 +1,49 @@
+package com.ruan.todosimple.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+    public interface CreateUser {}
+    public interface UpdateUser {}
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique= true, nullable= false)
+    @NotNull(groups= CreateUser.class)
+    @NotEmpty(groups= CreateUser.class)
+    @Size(groups= CreateUser.class, min=2, max=100)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name= "password", nullable= false)
+    @NotNull(groups= {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups= {CreateUser.class, UpdateUser.class})
+    @Size(groups= {CreateUser.class, UpdateUser.class}, min=8, max=60)
+    private String password;
+
+    // private List<Task> tasks = new ArrayList<Task>();
+}
