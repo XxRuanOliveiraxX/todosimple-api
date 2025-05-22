ef490279130774d712052240e8d2fe7919891eb4
XxRuanOliveiraxX <162846693+XxRuanOliveiraxX@users.noreply.github.com>
2025-05-22 18:53:57 -0300

    UserService updated

--- src/main/java/com/ruan/todosimple/services/UserService.java
+++ src/main/java/com/ruan/todosimple/services/UserService.java
@@ -1,5 +1,6 @@
 package com.ruan.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class UserService {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(()-> new RuntimeException(
             "Usuário não encontrado"));
+    }
+
+    public List<User> findAll(){
+    List<User> user = this.userRepository.findAll();
+    return user;
     }
 
     @Transactional
