a193537bb6468735a7da8af70a27502bb904ec33
XxRuanOliveiraxX <162846693+XxRuanOliveiraxX@users.noreply.github.com>
2025-05-23 20:14:23 -0300

    code updated

--- src/main/java/com/ruan/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/ruan/todosimple/configs/SecurityConfig.java
@@ -2,18 +2,24 @@ package com.ruan.todosimple.configs;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.ruan.todosimple.security.JWTUtil;
 
 //import com.lucasangelo.todosimple.security.JWTAuthenticationFilter;
 //import com.lucasangelo.todosimple.security.JWTAuthorizationFilter;
@@ -24,13 +30,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-        // private AuthenticationManager authenticationManager;
+        private AuthenticationManager authenticationManager;
 
-        // @Autowired
-        // private UserDetailsService userDetailsService;
+        @Autowired
+        private UserDetailsService userDetailsService;
 
-        // @Autowired
-        // private JWTUtil jwtUtil;
+        @Autowired
+        private JWTUtil jwtUtil;
 
         private static final String[] PUBLIC_MATCHERS = {
                         "/"
@@ -46,11 +52,11 @@ public class SecurityConfig {
                 http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                                 .csrf(csrf -> csrf.disable());
 
-                // AuthenticationManagerBuilder authenticationManagerBuilder = http
-                // .getSharedObject(AuthenticationManagerBuilder.class);
-                // authenticationManagerBuilder.userDetailsService(this.userDetailsService)
-                // .passwordEncoder(bCryptPasswordEncoder());
-                // this.authenticationManager = authenticationManagerBuilder.build();
+                AuthenticationManagerBuilder authenticationManagerBuilder = http
+                                .getSharedObject(AuthenticationManagerBuilder.class);
+                authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+                                .passwordEncoder(bCryptPasswordEncoder());
+                this.authenticationManager = authenticationManagerBuilder.build();
 
                 http.authorizeHttpRequests(auth -> auth
                                 .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
