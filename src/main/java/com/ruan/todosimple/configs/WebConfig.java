5233b97acb02293e6850ed0c8abe253d30cd8854
XxRuanOliveiraxX <162846693+XxRuanOliveiraxX@users.noreply.github.com>
2025-05-25 19:33:31 -0300

    front end code updated

--- src/main/java/com/ruan/todosimple/configs/WebConfig.java
+++ src/main/java/com/ruan/todosimple/configs/WebConfig.java
@@ -8,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
     }
+
 }
